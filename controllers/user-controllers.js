a125098e5f5fe0da20eaa71f9daa1191b3892a07
ArtemRodnaev <ihtiandr123.com@yandex.ru>
2025-02-24 15:04:32 +0500

    get users

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -135,7 +135,9 @@ class UserController {
           .status(404)
           .json({ type: "error", value: "Пользователей нет" });
 
-      const usersDTO = users.map((e) => DTO(e));
+      const usersDTO = users.map((e) => {
+        if (e.name) DTO(e);
+      });
       res.status(200).json({ type: "success", value: usersDTO });
     } catch (e) {
       return res.status(500).json({ message: e.message });
