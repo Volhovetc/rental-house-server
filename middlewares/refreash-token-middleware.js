97c112c9587e022cbaed3660437bc7adb2ae6f94
ArtemRodnaev <ihtiandr123.com@yandex.ru>
2025-02-25 17:11:30 +0500

    autoRefreash token 2.0

--- middlewares/refreash-token-middleware.js
+++ middlewares/refreash-token-middleware.js
@@ -1,8 +1,10 @@
 const jwt = require("jsonwebtoken");
 module.exports = function (req, res, next) {
   try {
-    console.log("Cookies: ", req.cookies);
-    console.log("Signed Cookies: ", req.signedCookies);
+    const a = JSON.parse(req.cookies);
+    const b = JSON.parse(req.signedCookies);
+    console.log("Cookies: ", a);
+    console.log("Signed Cookies: ", b);
     next();
   } catch (err) {
     res.status(500).json({ type: "error", value: "Ошибка сервера" });
